21830904373367dde3442503e79bc5b683a8e20f
F-Kibanov <102605381+F-Kibanov@users.noreply.github.com>
2023-02-07 01:30:36 +0500

    Add files via upload

---
+++ dz_7/model.py
@@ -0,0 +1,24 @@
+def get_x():
+    x = input("X: ")
+    return float(x)
+
+
+def get_y():
+    y = input("Y: ")
+    return float(y)
+
+
+def summ(x, y):
+    return x + y
+
+
+def sub(x, y):
+    return x - y
+
+
+def mult(x, y):
+    return x * y
+
+
+def div(x, y):
+    return x / y
